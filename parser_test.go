8386ef3ddfd35584a37350667e9093bf54b6aa4d
Rafal Jeczalik <rjeczalik@gmail.com>
2016-06-20 17:01:22 +0200

    parser: change test-case to use CustomClaims

--- parser_test.go
+++ parser_test.go
@@ -26,6 +26,12 @@ func init() {
 	jwtTestDefaultKey = test.LoadRSAPublicKeyFromDisk("test/sample_key.pub")
 }
 
+type CustomClaims map[string]interface{}
+
+func (cc CustomClaims) Valid() error {
+	return jwt.MapClaims(cc).Valid()
+}
+
 var jwtTestData = []struct {
 	name        string
 	tokenString string
@@ -39,7 +45,7 @@ var jwtTestData = []struct {
 		"basic",
 		"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJmb28iOiJiYXIifQ.FhkiHkoESI_cG3NPigFrxEk9Z60_oXrOT2vGm9Pn6RDgYNovYORQmmA0zs1AoAOf09ly2Nx2YAg6ABqAYga1AcMFkJljwxTT5fYphTuqpWdy4BELeSYJx5Ty2gmr8e7RonuUztrdD5WfPqLKMm1Ozp_T6zALpRmwTIW0QPnaBXaQD90FplAg46Iy1UlDKr-Eupy0i5SLch5Q-p2ZpaL_5fnTIUDlxC3pWhJTyx_71qDI-mAA_5lE_VdroOeflG56sSmDxopPEG3bFlSu1eowyBfxtu0_CuVd-M42RU75Zc4Gsj6uV77MBtbMrf4_7M_NUTSgoIF3fRqxrj0NzihIBg",
 		defaultKeyFunc,
-		jwt.MapClaims{"foo": "bar"},
+		&CustomClaims{"foo": "bar"},
 		true,
 		0,
 		nil,
@@ -197,6 +203,8 @@ func TestParser_Parse(t *testing.T) {
 			token, err = parser.ParseWithClaims(data.tokenString, jwt.MapClaims{}, data.keyfunc)
 		case *jwt.StandardClaims:
 			token, err = parser.ParseWithClaims(data.tokenString, &jwt.StandardClaims{}, data.keyfunc)
+		case *CustomClaims:
+			token, err = parser.ParseWithClaims(data.tokenString, &CustomClaims{}, data.keyfunc)
 		}
 
 		// Verify result matches expectation
